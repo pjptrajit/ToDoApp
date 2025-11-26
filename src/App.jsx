a83f2ca8e345d02478d576fb9a6d94b113af3e05
Rajit PrajaPati <pjptrajit@gmail.com>
2025-11-26 11:38:41 +0545

    restor  the edittodopage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Todo from './pages/Todo'
 import Home from './pages/Home'
 import Header from './components/Header'
+import EditToDo from './pages/EditToDo'
 
 function App() {
   // localStorage.setItem("name", "Rajit Prajapati");
@@ -15,7 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todo" element={<Todo />} />
-        {/* <Route index element={<Todo />} /> */}
+        <Route index element={<Todo />} />
+        <Route path="/edittodo" element={<EditToDo />} />
         <Route path="*" element={<h1>Path not found !</h1>}/>
       </Routes>
     </div>
